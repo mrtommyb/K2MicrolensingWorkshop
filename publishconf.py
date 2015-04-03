e1e36dec1f3cced0940041a366d9fb3c6dd00d5c
Tom Barclay <tom@tombarclay.com>
2015-04-02 22:06:21 -0700

    fixing footer

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = '/K2MicrolensingWorkshop'
+SITEURL = 'http://keplerscience.arc.nasa.gov/K2/K2MicrolensingWorkshop'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
